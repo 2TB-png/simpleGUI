ac6a12d90f2e3318dc8cae02df84d0b65ba3ee4e
2TB-png <nikola.cat1000@gmail.com>
2025-12-04 10:48:37 +0100

    New file :)
    I will install git soon...

---
+++ simpleGUI/element.py
@@ -0,0 +1,46 @@
+import pygame
+
+
+class Element:
+    def __init__(self, x: int, y: int, width: int, height: int, el_type: str = "element"):
+
+        self.__rect = pygame.Rect(x, y, width, height)
+        self.default_screen = None
+
+        self.type = el_type
+        self.tags = []
+
+    def update(self, screen = None):
+        if screen is None:
+            screen = self.default_screen
+        ...
+
+    def set_screen(self, screen):
+        self.default_screen = screen
+
+    def move(self, x, y):
+        self.__rect.x = x
+        self.__rect.y = y
+
+    def get_pos(self):
+        return self.__rect.x, self.__rect.y
+
+    def get_rect(self):
+        return self.__rect
+
+    def get_id(self):
+        return id(self)
+
+    def render_debug(self, screen = None, debug_color=(255, 0, 0), border_width=2):
+        if screen is None:
+            screen = self.default_screen
+
+        pygame.draw.rect(screen, debug_color, self.__rect, border_width)
+
+
+
+    def __str__(self):
+        return f"<Element of type {self.type} at {(self.__rect.x, self.__rect.y)}>"
+
+    def __repr__(self):
+        return f"<{self.type}( pos={(self.__rect.x, self.__rect.y)}, width={self.__rect.w}, height={self.__rect.h})>"
---
+++ simpleGUI/util.py
@@ -0,0 +1,31 @@
+import simpleGUI.origen, simpleGUI.userInput
+import pygame
+
+
+def generate_button_grid_from_matrix(matrix, width, height, font, separator: float = 0.1) -> simpleGUI.origen.Origen:
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    vertical_sep = height//rows*separator
+    horizontal_sep = width//cols*separator
+
+    button_height = height//rows - vertical_sep
+    button_width = width//cols - horizontal_sep
+
+    buttons = simpleGUI.origen.Origen(0, 0)
+
+    for row in range(rows):
+        for col in range(cols):
+
+            if matrix[row][col] is None:
+                continue
+
+            buttons.add_elements(
+                simpleGUI.userInput.Button(button_width * col + horizontal_sep * (col + 1), button_height * row + vertical_sep * (row + 1),
+                                           button_width, button_height,
+                                           matrix[row][col], font
+                                           )
+
+            )
+
+    return buttons
